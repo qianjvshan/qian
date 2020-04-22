b8364a3be3cb45d0d98e97570c8b54473c948e65
qian <1518915977@qq.com>
2020-04-22 11:36:10 +0800

    新建项目day03框架

---
+++ src/main/java/cn/jiyun/pojo.java
@@ -0,0 +1,23 @@
+package cn.jiyun;
+
+public class pojo {
+
+    private String uname;
+    private Integer age;
+
+    public String getUname() {
+        return uname;
+    }
+
+    public void setUname(String uname) {
+        this.uname = uname;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+}
